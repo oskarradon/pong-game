42c65858f82d559697feed635a8aba4b2fb48b6b
Oskar <oskarradonkimball@gmail.com>
2016-08-31 14:56:56 -0700

    Infinite x bounce loop

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const gameWidth    = 24;
 const gameHeight   = 18;
 const canvasWidth  = canvas.width;
 const canvasHeight = canvas.height;
+const gameSpeed    = 100;
 const gameState = {
-  ball: {x: gameWidth/2, y: gameHeight/2}
+  ball: {x: gameWidth/2, y: gameHeight/2, velocity: { x: 1, y: 1 }}
 };
 
 
@@ -18,6 +19,7 @@ gameState.getScreenBuffer = function() {
   for(var i=0; i<gameWidth; i++) {
     screenBuffer.push((new Array(gameHeight)).fill(false));
   }
+  console.log(this.ball.x);
   screenBuffer[this.ball.x][this.ball.y] = true;
   return screenBuffer;
 }
@@ -50,7 +52,7 @@ function step(timestamp) {
   if (!previousTimestamp) timestamp = timestamp;
   animationProgress += timestamp - previousTimestamp;
   previousTimestamp = timestamp;
-  if (animationProgress > 100) {
+  if (animationProgress > gameSpeed) {
     update(gameState);
     animationProgress = 0;
   }
@@ -59,7 +61,13 @@ function step(timestamp) {
 }
 
 function update(gameState) {
-  gameState.ball.x +=1;
+  if (gameState.ball.x === gameWidth - 1) {
+    gameState.ball.velocity.x *= -1;
+  }
+  if (gameState.ball.x === 0) {
+    gameState.ball.velocity.x *= -1;
+  }
+  gameState.ball.x += gameState.ball.velocity.x;
 }
 
 // init
