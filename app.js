e0c334f09aec6fde43d7eb220a00c8ed5d52a662
Oskar <oskarradonkimball@gmail.com>
2016-08-31 14:11:22 -0700

    Got ball object to move and made gameState obj

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "black";
+// ctx.fillStyle = "black";
 // ctx.fillRect(10, 10, 100, 100);
 
 const gameWidth    = 24;
 const gameHeight   = 18;
 const canvasWidth  = canvas.width;
 const canvasHeight = canvas.height;
+const gameState = {
+  ball: {x: gameWidth/2, y: gameHeight/2}
+};
 
 
-// game state 2d array of booleans
-let gameState = [];
-for(var i=0; i<gameWidth; i++) {
-  gameState.push((new Array(gameHeight)).fill(false));
+gameState.getScreenBuffer = function() {
+  const screenBuffer = [];
+  for(var i=0; i<gameWidth; i++) {
+    screenBuffer.push((new Array(gameHeight)).fill(false));
+  }
+  screenBuffer[this.ball.x][this.ball.y] = true;
+  return screenBuffer;
 }
 
-
 // draw function
-function draw(gameState) {
-  for(var i=0; i<gameState.length; i++) {
-    for(var j=0; j<gameState[i].length; j++) {
-      if(gameState[i][j]) {
+function draw(screenBuffer) {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,canvasWidth,canvasHeight);
+  ctx.fillStyle = "black";
+  for(var i=0; i<screenBuffer.length; i++) {
+    for(var j=0; j<screenBuffer[i].length; j++) {
+      if(screenBuffer[i][j]) {
         drawPixel(i,j);
       }
     }
@@ -34,6 +42,16 @@ function drawPixel(x,y) {
   ctx.fillRect(x*pixelWidth, y*pixelHeight, pixelWidth, pixelHeight);
 }
 
-gameState[1][1] = true;
+// animation loop
+function step(timestamp) {
+  update(gameState);
+  draw(gameState.getScreenBuffer());
+  window.requestAnimationFrame(step);
+}
 
-draw(gameState);
+function update(gameState) {
+  gameState.ball.x +=1;
+}
+
+// init
+window.requestAnimationFrame(step);
