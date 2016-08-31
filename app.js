284ab632a05f631793ffdfa852975bdf4b1774b5
Oskar <oskarradonkimball@gmail.com>
2016-08-31 15:14:13 -0700

    Draw paddles

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const canvasWidth  = canvas.width;
 const canvasHeight = canvas.height;
 const gameSpeed    = 100;
 const gameState = {
-  ball: {x: gameWidth/2, y: gameHeight/2, velocity: { x: 1, y: 1 }}
+  ball: {x: gameWidth/2, y: gameHeight/2, velocity: { x: 0, y: 0 }},
+  paddle1: {x: 0, y: gameHeight/2, velocity: { x: 0, y: 0 }},
+  paddle2: {x: gameWidth-1, y: gameHeight/2, velocity: { x: 0, y: 0 }}
 };
 
 
@@ -19,8 +21,18 @@ gameState.getScreenBuffer = function() {
   for(var i=0; i<gameWidth; i++) {
     screenBuffer.push((new Array(gameHeight)).fill(false));
   }
-  console.log(this.ball.x);
+  // set ball pixels to true
   screenBuffer[this.ball.x][this.ball.y] = true;
+  // set paddle1 pixels
+  screenBuffer[this.paddle1.x][this.paddle1.y] = true;
+  screenBuffer[this.paddle1.x][this.paddle1.y+1] = true;
+  screenBuffer[this.paddle1.x][this.paddle1.y+2] = true;
+  screenBuffer[this.paddle1.x][this.paddle1.y+3] = true;
+  // set paddle2 pixels
+  screenBuffer[this.paddle2.x][this.paddle2.y] = true;
+  screenBuffer[this.paddle2.x][this.paddle2.y+1] = true;
+  screenBuffer[this.paddle2.x][this.paddle2.y+2] = true;
+  screenBuffer[this.paddle2.x][this.paddle2.y+3] = true;
   return screenBuffer;
 }
 
@@ -67,7 +79,14 @@ function update(gameState) {
   if (gameState.ball.x === 0) {
     gameState.ball.velocity.x *= -1;
   }
+  if (gameState.ball.y === gameHeight - 1) {
+    gameState.ball.velocity.y *= -1;
+  }
+  if (gameState.ball.y === 0) {
+    gameState.ball.velocity.y *= -1;
+  }
   gameState.ball.x += gameState.ball.velocity.x;
+  gameState.ball.y += gameState.ball.velocity.y;
 }
 
 // init
